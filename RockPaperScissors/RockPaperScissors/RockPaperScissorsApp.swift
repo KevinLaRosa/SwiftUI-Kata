bc5d3360071fdb4ac9e05db454fea6734996d06c
Kévin La Rosa <contact@kevinlarosa.fr>
2025-05-08 09:01:09 +0200

    init RockPaperScissors

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Roger La Rosa on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
